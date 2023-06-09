1088d69c28c3b3f206df1a3fb2586348a536b353
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2023-06-09 21:39:57 +0530

    add thirdweb wallet button

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,40 @@ import Dashboard from "./Pages/Dashboard";
 import Header from "./Components/Header/Header";
 import LandingPage from "./Pages/Landing";
 import Footer from "./Components/Footer/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import OffsetView from "./Pages/Offset";
 import SwapView from "./Pages/Swap";
 import OrderConfirmationPage from "./Components/Carbon/OrderConfrimation";
+import { useConnectionStatus } from "@thirdweb-dev/react";
+import { ThirdwebProvider, metamaskWallet } from "@thirdweb-dev/react";
 
 const App: React.FC = () => {
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/offset" element={<OffsetView />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/swap" element={<SwapView />} />
-          <Route path="/confirmOrder" element={<OrderConfirmationPage />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <ThirdwebProvider
+        supportedWallets={[metamaskWallet()]}
+        activeChain="mumbai"
+      >
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/offset" element={<OffsetView />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/swap" element={<SwapView />} />
+            <Route path="/confirmOrder" element={<OrderConfirmationPage />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </ThirdwebProvider>
     </>
   );
 };
 
+
+const ProtectedRoute = ({ element, ...rest }: any) => {
+  const connectionStatus = useConnectionStatus();
+
+  return connectionStatus === "disconnected" ? <Route {...rest} element={element} /> : <Navigate to="/" />;
+};
 export default App;
